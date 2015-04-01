dca03a4a0f16d8437d8739e9f55cd1283e88a290
monkinto <master.monkinto@gmail.com>
2015-04-01 11:44:49 -0400

    Added files to assignment 8

---
+++ assignment8/BinaryTree.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BinaryTree.h"
+
+int hasLeft(NodePtr node){
+	return 0;
+}
+
+int hasRight(NodePtr node){
+	return 0;
+}
+
+int isLeaf(NodePtr node){
+	return 0;
+}
+
+BinTreePtr initBinaryTree(){
+	return NULL;
+}
+
+NodePtr makeNode(Data theData){
+	return NULL;
+}
+
+int isEmpty(BinTreePtr tree){
+	return 0;
+}
+
+NodePtr addRoot(BinTreePtr tree, Data theData){
+	return NULL;
+}
+
+NodePtr addChild(NodePtr Parent, int leftOrRightChild, Data theData){
+	return NULL;
+}
+
+NodePtr emptySubtree(NodePtr node){
+	return NULL;
+}
+
+BinTreePtr destroyTree(BinTreePtr tree){
+	return NULL;
+}
+
+int numNodesSubtree(NodePtr node){
+	return 0;
+}
+
+int numLeavesSubTree(NodePtr node){
+	return 0;
+}
+
+int heightSubtree(NodePtr node){
+	return 0;
+}
---
+++ assignment8/guessingGame.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BinaryTree.h"
+
+void fillDecisionTree(BinTreePtr tree){
+	
+}
+
+void play(BinTreePtr tree){
+	
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
